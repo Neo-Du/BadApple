20cd3a25e95fcdaee98eb3b7500199abb303b678
Du <duzlvip@gmail.com>
2020-09-20 16:33:05 +0800

    create project

---
+++ VisualStudio/BadApple/main.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+	waitKey(0);
+	return 0;
+}
