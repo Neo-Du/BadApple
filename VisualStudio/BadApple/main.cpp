ff062489c902b741d9a890547ff75b0251245227
Du <duzlvip@gmail.com>
2020-09-21 21:59:12 +0800

    get 2 bin file , start check with SD card

--- VisualStudio/BadApple/main.cpp
+++ VisualStudio/BadApple/main.cpp
@@ -12,100 +12,182 @@ using namespace cv;
 using namespace std;
 
 
-#define WINDOW1 "Input_Video"
-#define WINDOW2 "Output_Video"
-#define OUTPUT_HEIGHT 320
-#define OUTPUT_WIDTH 240
 
-char srcFile[] = "../../Video/BadApple.mp4";
+#define OUTPUT_HEIGHT 640
+#define OUTPUT_WIDTH 480
+
+char video_path[] = "../../Video/BadApple.mp4";
+char test_pic_path[] = "tset.jpg";
+char test_pic2_path[] = "tset2.jpg";
+
 
 VideoCapture srcVideo = NULL;
-Mat SrcFrame,OutFrame;
-ofstream outFile("out.bin",ios::out|ios::binary);
+ofstream outFile("out.bin", ios::out | ios::binary);
 Mat testPic2 = imread("tset.jpg");
 Mat testPic;
-void Bin_output()
+void Bin_output(char* path)
 {
-	ofstream file2("out2.bin", ios::binary);
-	uint16_t a = 0xffff;
-	uint16_t b = 0xffcc;
-	uint16_t c = 0xabc0;
-	uint8_t d1 = (uint8_t)c;
-	uint8_t d2 = c>>8;
+	ofstream bin_out("out3.bin", ios::binary);
+	int b, g, r, total;
+	total = 0;
+	Mat srcImage, outImage;
+	Size dsize = Size(OUTPUT_HEIGHT, OUTPUT_WIDTH);
+	uint8_t d1, d2;
+	uint16_t n;
+	srcImage = imread(path);
+	imshow("srcImage", srcImage);
+	resize(srcImage, outImage, dsize);
+	imshow("outImage", outImage);
 
-	file2.write((char*)&a, sizeof(a));
-	file2.write((char*)&b, sizeof(b));
-	file2.write((char*)&c, sizeof(c));
-
-	file2.write((char*)&d2, sizeof(d2));
-	file2.write((char*)&d1, sizeof(d1));
-
-	file2.close();
+	for (int i = 0; i < outImage.rows; i++)
+	{
+		for (int j = 0; j < outImage.cols; j++)
+		{
+			b = outImage.at<Vec3b>(i, j)[0] >> 3;
+			g = outImage.at<Vec3b>(i, j)[1] >> 2;
+			r = outImage.at<Vec3b>(i, j)[2] >> 3;
+			n = b << 11 | g << 5 | r;
+			
+			d1 = (uint8_t)n;
+			d2 = n >> 8;
+			bin_out.write((char*)&d2, sizeof(d2));
+			bin_out.write((char*)&d1, sizeof(d1));
+		}
+	}
+	bin_out.close();
 }
 
-void pic2RGB565()
+void Bin_output_2pic(char* path, char* path2)
+{
+	ofstream bin_out("out4_2pic.bin", ios::binary);
+	int b, g, r, total;
+	total = 0;
+	Mat srcImage,srcImage2, outImage, outImage2;
+	Size dsize = Size(OUTPUT_HEIGHT, OUTPUT_WIDTH);
+	uint8_t d1, d2;
+	uint16_t n;
+	srcImage = imread(path);
+	srcImage2 = imread(path2);
+	
+	resize(srcImage, outImage, dsize);
+	resize(srcImage2, outImage2, dsize);
+
+	imshow("srcImage", srcImage);
+	imshow("srcImage2", srcImage2);
+	imshow("outImage", outImage);
+	imshow("outImage", outImage2);
+
+	
+	for (int i = 0; i < outImage.rows; i++)
+	{
+		for (int j = 0; j < outImage.cols; j++)
+		{
+			b = outImage.at<Vec3b>(i, j)[0] >> 3;
+			g = outImage.at<Vec3b>(i, j)[1] >> 2;
+			r = outImage.at<Vec3b>(i, j)[2] >> 3;
+			n = b << 11 | g << 5 | r;
+
+			d1 = (uint8_t)n;
+			d2 = n >> 8;
+			bin_out.write((char*)&d2, sizeof(d2));
+			bin_out.write((char*)&d1, sizeof(d1));
+		}
+	}
+
+	for (int i = 0; i < outImage2.rows; i++)
+	{
+		for (int j = 0; j < outImage2.cols; j++)
+		{
+			b = outImage2.at<Vec3b>(i, j)[0] >> 3;
+			g = outImage2.at<Vec3b>(i, j)[1] >> 2;
+			r = outImage2.at<Vec3b>(i, j)[2] >> 3;
+			n = b << 11 | g << 5 | r;
+
+			d1 = (uint8_t)n;
+			d2 = n >> 8;
+			bin_out.write((char*)&d2, sizeof(d2));
+			bin_out.write((char*)&d1, sizeof(d1));
+		}
+	}
+	bin_out.close();
+}
+void pic_to_array(char* path)
 {
 	int b, g, r, total, n;
 	total = 0;
+	Mat srcImage, outImage;
 	Size dsize = Size(OUTPUT_HEIGHT, OUTPUT_WIDTH);
-	resize(testPic2, testPic, dsize);
-	imshow(WINDOW2, testPic);
-	//outFile << "const uint16_t pic_array [640*480]= {";
-	for (int i = 0; i < testPic.rows; i++)
+	ofstream p2a("out.txt");
+
+	srcImage = imread(path);
+	imshow("srcImage", srcImage);
+	resize(srcImage, outImage, dsize);
+	imshow("outImage", outImage);
+
+
+	p2a << "const uint16_t pic_array[" << OUTPUT_HEIGHT * OUTPUT_WIDTH << "]= {";
+	for (int i = 0; i < outImage.rows; i++)
 	{
-		for (int j = 0; j < testPic.cols; j++)
+		for (int j = 0; j < outImage.cols; j++)
 		{
-			b = testPic.at<Vec3b>(i, j)[0] >> 3;
-			g = testPic.at<Vec3b>(i, j)[1] >> 2;
-			r = testPic.at<Vec3b>(i, j)[2] >> 3;
+			b = outImage.at<Vec3b>(i, j)[0] >> 3;
+			g = outImage.at<Vec3b>(i, j)[1] >> 2;
+			r = outImage.at<Vec3b>(i, j)[2] >> 3;
 
 			if (total % 20 == 0)
 			{
-				outFile << endl;
+				p2a << endl;
 			}
 			n = b << 11 | g << 5 | r;
-			/*outFile << ios::binary << n ;*/
-			outFile.write((char *)&n,1);
-			
+			p2a << "0x" << hex << n << ',';
 			total++;
 		}
 	}
-	//outFile << endl << "};" << endl;
-	outFile.close();
+	p2a << endl << "};" << endl;
+	p2a.close();
+}
+
+void play_video(char* path)
+{
+	Mat SrcFrame, OutFrame;
+	Size dsize = Size(OUTPUT_HEIGHT, OUTPUT_WIDTH);
+
+	namedWindow("SourceVideo", WINDOW_AUTOSIZE);
+	namedWindow("OutVideo", WINDOW_AUTOSIZE);
+	srcVideo = VideoCapture(path);
+	if (!srcVideo.isOpened())
+	{
+		cout << "Error, can not open file" << endl;
+	}
+	cout << "Source video Width=" << srcVideo.get(CAP_PROP_FRAME_WIDTH)
+		<< "  Height=" << srcVideo.get(CAP_PROP_FRAME_HEIGHT)
+		<< " FPS : " << srcVideo.get(CAP_PROP_FPS)
+		<< endl;
+
+	int delay = 1000 / srcVideo.get(CAP_PROP_FPS);
+
+
+	for (;;)
+	{
+		srcVideo >> SrcFrame;
+		if (SrcFrame.empty())
+		{
+			break;
+		}
+		imshow("SourceVideo", SrcFrame);
+		resize(SrcFrame, OutFrame, dsize);
+		imshow("OutVideo", OutFrame);
+		waitKey(delay);
+	}
 }
 
 int main()
 {
-	//namedWindow(WINDOW1, WINDOW_AUTOSIZE);
-	//namedWindow(WINDOW2, WINDOW_AUTOSIZE);
-	//srcVideo = VideoCapture(srcFile);
-	//if (!srcVideo.isOpened())
-	//{
-	//	cout << "Error, can not open file" << endl;
-	//}
-	//cout<< "Source video Width=" << srcVideo.get(CAP_PROP_FRAME_WIDTH)
-	//	<< "  Height=" << srcVideo.get(CAP_PROP_FRAME_HEIGHT)
-	//	<< " FPS : " << srcVideo.get(CAP_PROP_FPS)
-	//	<< endl;
-	//int delay = 1000 / srcVideo.get(CAP_PROP_FPS);
-	//Size dsize = Size(OUTPUT_HEIGHT, OUTPUT_WIDTH);
+	//play_video(srcFile);
+	//pic_to_array(test_pic_path);
+	//Bin_output(test_pic_path);
 
-	//for (;;)
-	//{
-	//	srcVideo >> SrcFrame;
-	//	if (SrcFrame.empty())
-	//	{
-	//		break;
-	//	}
-	//	imshow(WINDOW1, SrcFrame);
-	//	resize(SrcFrame, OutFrame,dsize);
-	//	imshow(WINDOW2, OutFrame);
-	//	waitKey(delay);
-	//}
-	
-	//pic2RGB565();
-
-	Bin_output();
+	Bin_output_2pic(test_pic_path,test_pic2_path);
 	cout << "done" << endl;
 	while (1);
 	waitKey(0);
